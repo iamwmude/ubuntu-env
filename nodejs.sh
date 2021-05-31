8e56001f589581c85295da11221d0e8c47469bd8
root <root@MooD-PC.localdomain>
2021-05-31 15:57:43 +0800

    add nodejs.sh - first commit

---
+++ nodejs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+VERSION=v14.17.0
+DISTRO=linux-x64
+
+# Unzip the binary archive to any directory you wanna install Node
+curl -o node-$VERSION-$DISTRO.tar.xz https://nodejs.org/dist/$VERSION/node-$VERSION-$DISTRO.tar.xz
+sudo mkdir -p /usr/local/lib/nodejs
+sudo tar -xJvf node-$VERSION-$DISTRO.tar.xz -C /usr/local/lib/nodejs
+rm node-$VERSION-$DISTRO.tar.xz
+
+# Set the environment variable ~/.profile, add below to the end
+echo "export PATH=/usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin:$PATH" >> ~/.profile
+
+# Refresh profile
+# . ~/.profile
