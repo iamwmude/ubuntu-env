172d167b5bdc9ff9d9d9941bdf3ff55f07c67ad2
Mu-De <iamwmude@gmail.com>
2021-06-09 10:26:38 +0800

    add link health check for refresh-vmware-shared-folder.sh

--- tool/refresh-vmware-shared-folder.sh
+++ tool/refresh-vmware-shared-folder.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo vmhgfs-fuse .host:/ /mnt/hgfs/ -o allow_other -o uid=1000
+LINK_STATUS=$(. check-symbolic-link-health.sh)
+
+if [ LINK_STATUS == 'Broken' ] ; then
+    sudo vmhgfs-fuse .host:/ /mnt/hgfs/ -o allow_other -o uid=1000
+fi
