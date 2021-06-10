fce86b5c4151e9936d6f3315f8d38ee8639b8e6d
Mu-De <iamwmude@gmail.com>
2021-06-10 16:19:29 +0800

    add relative path to LINK_STATUS

--- tool/refresh-vmware-shared-folder.sh
+++ tool/refresh-vmware-shared-folder.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-LINK_STATUS=$(. check-symbolic-link-health.sh)
+DIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+LINK_STATUS=$(. $DIR/check-symbolic-link-health.sh)
 
-if [ LINK_STATUS == 'Broken' ] ; then
+if [ $LINK_STATUS == 'Broken' ] ; then
     sudo vmhgfs-fuse .host:/ /mnt/hgfs/ -o allow_other -o uid=1000
+    echo 'Done'
 fi
